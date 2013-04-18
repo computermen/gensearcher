4bda32f24114e6e52ccda627a4393b2c46047e15
jangus <computermen@slingshot.co.nz>
2013-04-18 14:02:47 +1200

    Now a useful Genealogical Name Searcher with option create search strings thatmay include reversing the order of Firstname(s) and surname.

--- MySecondApp/src/com/example/mysecondapp/MainActivity.java
+++ MySecondApp/src/com/example/mysecondapp/MainActivity.java
@@ -10,10 +10,10 @@ import android.webkit.WebView;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-private static final String EditText = null;
+// private static final String EditText = null;
 	TextView textView;
 	EditText firstnameeditText, surnameeditText;
-	// CheckBox pepBox, cheeseBox;
+	CheckBox reverseBox;
 	WebView webView;
 	
     @Override
@@ -21,8 +21,7 @@ private static final String EditText = null;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // pepBox = (CheckBox) findViewById(R.id.checkBox1);
-        // cheeseBox = (CheckBox) findViewById(R.id.checkBox2);
+        reverseBox = (CheckBox) findViewById(R.id.checkBox1);
         textView = (TextView) findViewById(R.id.textView2);
         firstnameeditText = (EditText) findViewById(R.id.editText1);
         surnameeditText = (EditText) findViewById(R.id.editText2);
@@ -39,23 +38,22 @@ private static final String EditText = null;
     // Hard coded strings need fixing
     public void onButton1Click(View view) {
     	StringBuilder str = new StringBuilder("");
-    	// if (pepBox.isChecked()) {
-    	//	str.append("Pepperoni");
-    	//}
-    	//if (cheeseBox.isChecked()) {
-    	//	str.append("\"Extra cheese\"");
-    	//}
-    	//if (str.length() == 23) {
-    	//	str.insert(9, '+');
-    	//}
-    	//if (str.length() == 0 ) {
-    	//	str.append("Plain");
-    	//}
     	str.append("\"");
     	str.append(firstnameeditText.getText());
     	str.append(" ");
     	str.append(surnameeditText.getText());
     	str.append("\"");
+    	if (reverseBox.isChecked()) {
+        	str.append(" ");
+        	str.append("OR");
+        	str.append(" ");
+        	str.append("\"");
+        	str.append(surnameeditText.getText());
+        	str.append(",");
+        	str.append(" ");
+        	str.append(firstnameeditText.getText());
+        	str.append("\"");
+        	}
     	textView.setText(str);
     	webView.loadUrl ("http://www.google.com/search?q=" + str.toString());
     }
