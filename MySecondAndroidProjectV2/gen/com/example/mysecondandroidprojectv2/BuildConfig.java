8ff0b548bbfa98c649328077871a58dd59497325
jangusnz <jangus@slingshot.co.nz>
2013-04-18 23:55:12 +1200

    Added hints for Firstname(s) and Surname fields etc.

--- MySecondAndroidProjectV2/gen/com/example/mysecondandroidprojectv2/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.mysecondandroidprojectv2;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
