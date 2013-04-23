3a16e3b78bb3c2d7e82ceac7d08999fb5091fa6d
jangus <computermen@slingshot.co.nz>
2013-04-23 13:55:24 +1200

    Added a "Married Name" field, plus an extra search term field with option to turn each option on/off.

--- MySecondApp/src/com/example/mysecondapp/MainActivity.java
+++ MySecondApp/src/com/example/mysecondapp/MainActivity.java
@@ -13,8 +13,8 @@ import nz.co.computermen.gensearcher.R;
 public class MainActivity extends Activity {
 // private static final String EditText = null;
 	TextView textView;
-	EditText firstnameeditText, surnameeditText;
-	CheckBox reverseBox;
+	EditText firstnameeditText, surnameeditText, marriednameText, othertermText;
+	CheckBox reverseBox, marriedBox, othertermBox;
 	WebView webView;
 	
     @Override
@@ -23,9 +23,13 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         reverseBox = (CheckBox) findViewById(R.id.checkBox1);
+        marriedBox = (CheckBox) findViewById(R.id.checkBox2);
+        othertermBox = (CheckBox) findViewById(R.id.checkBox3);
         textView = (TextView) findViewById(R.id.textView2);
         firstnameeditText = (EditText) findViewById(R.id.editText1);
         surnameeditText = (EditText) findViewById(R.id.editText2);
+        marriednameText = (EditText) findViewById(R.id.editText3);
+        othertermText = (EditText) findViewById(R.id.editText4);
         webView = (WebView) findViewById(R.id.webView1);
     }
 
@@ -44,6 +48,17 @@ public class MainActivity extends Activity {
     	str.append(" ");
     	str.append(surnameeditText.getText());
     	str.append("\"");
+    	if (marriedBox.isChecked()) {
+    		str.append(" ");
+        	str.append("OR");
+        	str.append(" ");
+    		str.append("\"");
+        	str.append(firstnameeditText.getText());
+        	str.append(" ");
+        	str.append(marriednameText.getText());
+        	str.append("\"");
+    		}
+    	
     	if (reverseBox.isChecked()) {
         	str.append(" ");
         	str.append("OR");
@@ -54,8 +69,25 @@ public class MainActivity extends Activity {
         	str.append(" ");
         	str.append(firstnameeditText.getText());
         	str.append("\"");
-        	}
+        	if (marriedBox.isChecked()) {
+        		str.append(" ");
+            	str.append("OR");
+            	str.append(" ");
+        		str.append("\"");
+        		str.append(marriednameText.getText());
+        		str.append(",");
+            	str.append(" ");
+            	str.append(firstnameeditText.getText());
+            	str.append("\"");
+        		}
+        
+    		}
+    	if (othertermBox.isChecked()) {
+        	str.append(" ");
+           	str.append(othertermText.getText());
+        }
     	textView.setText(str);
     	webView.loadUrl ("http://www.google.com/search?q=" + str.toString());
     }
-}
+ }
+
